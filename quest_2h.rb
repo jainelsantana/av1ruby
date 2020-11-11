49df7638c749e257278240c3226f4fbe7498c4c7
jainelsantana <jainel@hotmail.com.br>
2020-11-11 17:43:38 -0300

    Create quest_2h.rb

---
+++ quest_2h.rb
@@ -0,0 +1,10 @@
+puts'digite comprimento: '
+comprimento = gets.chomp.to_f
+puts'digite largura: '
+largura = gets.chomp.to_f
+puts'digite altura: '
+altura = gets.chomp.to_f
+
+volume = comprimento*largura*altura
+
+puts"volume é #{volume}"
