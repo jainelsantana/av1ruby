61e225231cb32b42f131d43fb80e9ffe3130abc3
jainelsantana <jainel@hotmail.com.br>
2020-11-11 16:35:53 -0300

    Create quest_1.rb

---
+++ quest_1.rb
@@ -0,0 +1,14 @@
+x=1
+a=3
+b=5
+c=8
+d=7
+puts (not x>3)
+puts (x<1) && (not b>d)
+puts (not x>3) || (c>5)
+puts (not x>3) || (c<7)
+puts (a>b) || (c>b)
+puts x>=2
+puts (x<1) && (b>=d)
+puts (d<0)|| (not b<7)
+puts (a>b) || (not c>b)
